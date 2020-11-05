d0614d5c7bade36e9c6f2d4b43478abc445c5471
Jinone <44525519+Jinone@users.noreply.github.com>
2020-11-05 10:59:07 +0800

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -1,6 +1,5 @@
-alert(origin);
 console.log('Frame Analytics Script Securely Loaded');
-console.log('User Agent String: ' + navigator.userAgent);
+console.log('User Agent String: ' + navigator.userAgent);alert(origin);
 console.log('User Agent Vendor: ' + navigator.vendor);
 console.log('User OS: ' + navigator.platform);
 console.log('User Language: ' + navigator.language);
