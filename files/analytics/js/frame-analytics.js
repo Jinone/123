d949646b3f520ceda58228947885d75b24b72dca
Jinone <44525519+Jinone@users.noreply.github.com>
2020-11-05 10:31:50 +0800

    Update frame-analytics.js

--- files/analytics/js/frame-analytics.js
+++ files/analytics/js/frame-analytics.js
@@ -3,4 +3,3 @@ console.log('User Agent String: ' + navigator.userAgent);
 console.log('User Agent Vendor: ' + navigator.vendor);
 console.log('User OS: ' + navigator.platform);
 console.log('User Language: ' + navigator.language);
-<svg/onload=alert(origin)>
